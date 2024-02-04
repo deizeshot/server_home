8ea3a193054a0285ca10dea425eeaee079efddde
deizeshot <deizeshot@gmail.com>
2024-02-04 17:25:01 +0300

    5122

--- uploads.php
+++ uploads.php
@@ -2,19 +2,6 @@
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-
-// Проверка, является ли файл изображением
-if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
-    }
-}
 
 // Проверка, существует ли уже файл с таким именем
 if (file_exists($target_file)) {
@@ -25,13 +12,6 @@ if (file_exists($target_file)) {
 // Проверка размера файла
 if ($_FILES["fileToUpload"]["size"] > 500000) {
     echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
-
-// Разрешенные форматы файлов
-$allowed_formats = array("jpg", "jpeg", "png", "gif");
-if(!in_array($imageFileType, $allowed_formats)) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
     $uploadOk = 0;
 }
 
