0e32a9cbc07692c527dff35c7f5937dc458796c3
deizeshot <deizeshot@gmail.com>
2024-02-04 16:02:15 +0300

    2

---
+++ files.php
@@ -0,0 +1,17 @@
+<?php
+$uploadsDirectory = 'uploads/';
+
+// Получаем список файлов в директории
+$files = scandir($uploadsDirectory);
+
+// Убираем из списка . и ..
+$files = array_diff($files, array('.', '..'));
+
+// Выводим список файлов
+echo "<h1>Список файлов:</h1>";
+echo "<ul>";
+foreach ($files as $file) {
+    echo "<li><a href='$uploadsDirectory$file' target='_blank'>$file</a></li>";
+}
+echo "</ul>";
+?>
