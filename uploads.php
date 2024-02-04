ca7e8b3b0240ad480bc445c4c4742dd415a5cdee
deizeshot <deizeshot@gmail.com>
2024-02-04 17:44:41 +0300

    512

--- uploads.php
+++ uploads.php
@@ -20,7 +20,7 @@ if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+        echo "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded.";
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
