72fa7a02beb442b677ed5802a9f4625529d62aea
deizeshot <deizeshot@gmail.com>
2024-02-05 10:34:27 +0300

    2

--- file_list.php
+++ file_list.php
@@ -10,9 +10,10 @@ $fileList = glob($uploadsDirectory . "*");
 foreach ($fileList as $file) {
     $fileName = basename($file);
     echo "<div class='file-item'>";
+    echo "<span class='file-icon'><i class='far fa-file'></i></span>";
     echo "<span>$fileName</span>";
-    echo "<button onclick='deleteFile(\"$fileName\")'>Delete</button>";
-    echo "<a href='download_file.php?file=$fileName' download><button>Download</button></a>";
+    echo "<button class='delete-button' onclick='deleteFile(\"$fileName\")'><i class='fas fa-trash'></i> Delete</button>";
+    echo "<button class='download-button' onclick='downloadFile(\"$fileName\")'><i class='fas fa-download'></i> Download</button>";
     echo "</div>";
 }
 ?>
