ed7f8e43da9d7a9a653c9b39df8d71b5e2484c0a
deizeshot <deizeshot@gmail.com>
2024-02-05 10:13:52 +0300

    1

--- uploads.php
+++ uploads.php
@@ -10,7 +10,7 @@ if (file_exists($target_file)) {
 }
 
 // Проверка размера файла
-if ($_FILES["fileToUpload"]["size"] > 500000) {
+if ($_FILES["fileToUpload"]["size"] > 50000000) {
     echo "Sorry, your file is too large.";
     $uploadOk = 0;
 }
